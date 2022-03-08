6755ec1414d47ddad0bdb37681ae2a06de3d1316
Bruno <bruno_151997@hotmail.com>
2022-03-07 22:30:02 -0300

    Adiciona home e inicia modal

---
+++ app.js
@@ -0,0 +1,30 @@
+const vm = new Vue({
+  el: "#app",
+  data: {
+    produtos: [],
+    produto: [],
+  },
+  filters: {
+    formataPreco(valor) {
+      return valor.toLocaleString("pt-br", {
+        style: "currency",
+        currency: "BRL",
+      });
+    },
+  },
+  methods: {
+    async fetchProducts() {
+      const response = await fetch("./api/produtos.json");
+      const json = await response.json();
+      this.produtos = json;
+    },
+    async fetchProduct(id) {
+      const response = await fetch(`./api/produtos/${id}/dados.json`);
+      const json = await response.json();
+      this.produto = json;
+    },
+  },
+  created() {
+    this.fetchProducts();
+  },
+});
